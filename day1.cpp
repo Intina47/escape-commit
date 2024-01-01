4fb4cb7dfc01d5537756a0bcd280e7886661dfa2
uzohou <isaiahntina47@gmail.com>
2024-01-01 19:45:12 +0000

    The one where i thought i knew everything

--- day1.cpp
+++ day1.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
-
+void greetings(std::string name){
+	std::printf("Nice to meet you %s.\nWelcome to my daily dairy! ", name.c_str());
+	std::cout << "My name is Ntina\n";
+}
 int main(int argc, char* argv[]){
 	std::string name;
 	if(argc < 2){
 		std::cout<< "Hello there nice to meet you, what is your name? " <<std::endl;
 		std::cin >> name;
-		std::cout << "Nice meeting you " << name <<std::endl;
+		greetings(name);
 	} else {
-		std::cout << "Nice to meet you " << argv[1] <<std::endl;
-		std::cout << "My name is Ntina\n";
+		greetings(argv[1]);
 	}
 	return 0;
 }
