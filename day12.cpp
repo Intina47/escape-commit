5cddf3c11f9a32905db15352590217da278f04d2
uzohou <isaiahntina47@gmail.com>
2024-01-14 18:09:18 +0000

    The one where i almost gave up but went all in

--- day12.cpp
+++ day12.cpp
@@ -12,10 +12,26 @@ int largestnum(int arr[], int n){
 	return largestnum;
 }
 
+int secondLargest(int arr[], int n){
+	int largestnum = arr[0];
+	int secondLargest = -1;
+	for(int i=0; i<n; ++i){
+		if(arr[i] > largestnum){
+			secondLargest = largestnum;
+			largestnum = arr[i];
+		}else if(arr[i] >= secondLargest && arr[i] != largestnum){
+			secondLargest = arr[i];
+		}
+	}
+	return secondLargest;
+}
+
 int main(){
 	int arr[] = {2,1,0,10,12,4};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	int result = largestnum(arr,n);
 	cout << result << endl;
+	int secondl = secondLargest(arr, n);
+	cout << secondl <<endl;
 	return 0;
 }
