a09d0154fc6b7a25bc90cd3c17bd702017aac601
mamba <78519682+Intina47@users.noreply.github.com>
2024-01-24 14:49:11 +0000

    search for a target in sorted arraay pg

--- pg.cpp
+++ pg.cpp
@@ -1,69 +1,43 @@
-//find the minimum in rotated sorted array
-//given a sorted array nums of unique elements , return the minimum element of this array
-//approach: Binary searcg TC: O(log n)
+// search sorted array for a target value
+// time complexity expected: o(log n)
+//return the index otherwise return -1
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-// merge sorte
-void bubbleSort(vector<int> &arr, int n){
-    for(int i=0; i<n-1; ++i){
-        for(int j=0; j<n-i-1; ++j){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+int search_for_target(const vector<int>& arr, int target, int n){
+    // lets implement binary search for the start
+    int l=0, r=n-1;
+    while(l<=r){
+        int mid = (l+r)/2;
+
+        if(target == arr[mid]){
+            return mid;
+        }
+
+        if(arr[l] <= arr[mid]){
+            if(target > arr[mid] or target < arr[l]){
+                l = mid + 1;
+            }else{
+                r = mid -1;
+            }
+        } else {
+            if(target < arr[mid] or target > arr[r]){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
             }
         }
     }
-}
-
-int minElement(vector<int>& arr, int n){
-    cout << "Finding minimum element from: ";
-    for(const auto& element : arr){
-        cout << element << ",";
-    }
-    int left = 0, right = n-1;
-    int minres = arr[0];
-    while(left < right){
-        if(arr[left] < arr[right]){
-            minres = min(minres, arr[left]);
-            break;
-        }
-
-        int mid = (left + right) /2;
-        minres = min(minres, arr[mid]);
-        if(arr[mid] >= arr[left] || arr[mid] == arr[left]){
-            left = mid + 1;
-        } else{
-            right = mid - 1;
-        }
-    }
-    return minres;
+    return -1;
 }
 
 int main(){
-    vector<int> arr = {3,7,9,8,4,0,5,6,1,2};
+    const vector<int> arr = {4,5,6,7,8,0,1,2,3};
     int n = arr.size();
-    bubbleSort(arr, n);
-    cout << endl;
-    cout << "Sorted: ";
-    for(int i=0; i<n; ++i){
-    cout << arr[i] << ",";
-    }
-    
-    cout << endl;
-     
-    // simple rotation to the right
-    std::rotate(arr.rbegin(), arr.rbegin() + 5, arr.rend());
-    cout << "rotated Array: ";
-    for(const auto& element: arr){
-        cout << element << ",";
-    }
-    cout << endl;
-    int smallnum  = minElement(arr,n);
-    cout << "Smallest Num: " <<  smallnum <<endl;
-    cout << endl;
+    int target = 9;
+    int result = search_for_target(arr,target,n);
+    cout << result <<endl;
+
     return 0;
 }
