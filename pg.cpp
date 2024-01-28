75610f24a53f425f0ee03b6622fe3b947aec2d12
mamba <78519682+Intina47@users.noreply.github.com>
2024-01-28 19:55:26 +0000

    PLAY GROUND ON TWO SUM

--- pg.cpp
+++ pg.cpp
@@ -5,26 +5,67 @@
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
-vector<int> twosum(vector<int> &arr, int n, int target){
-    unordered_map<int,int> ht ;
-    for(int i=0; i<n; ++i){
+// two sum
+vector<int> twosum(const vector<int>& arr, int target){
+    unordered_map<int, int> seen;
+    for(int i=0; i<arr.size(); ++i){
         int complement = target - arr[i];
-        if(ht.count(complement)){
-            return {ht[complement], i};
+        if(!seen.count(complement)){
+            seen[arr[i]] = i;
+        }else{
+            return {seen[complement], i};
         }
-        ht[arr[i]] = i;
     }
-    return {};
+    return {0,0};
 }
 
-int  main(){
-    vector<int> arr = {2,4,5,6,8,1};
-    int n = arr.size();
-    vector<int> ar = twosum(arr, n, 14);
-    for(const auto& element : ar){
-        cout << element << ", ";
+// happy number
+/**/
+
+int digitSquareSum(int n){
+    cout << "Our N: "<< n <<endl;
+    int sum = 0;
+    while(n > 0){
+        int digit = n % 10;
+        cout << "OUr Digit: " << digit <<endl;
+        sum += digit * digit;
+        cout << "Our Sum: " << sum <<endl;
+        n /= 10;
     }
-return 0;
+    return sum;
+}
+
+bool isHappy(int n) {
+    std::unordered_set<int> seen;
+
+    while (n != 1 && seen.find(n) == seen.end()) {
+        seen.insert(n);
+        n = digitSquareSum(n);
+    }
+
+    return n == 1;
+}
+
+int main() {
+    // int num = 19;
+
+    // if (isHappy(num)) {
+    //     std::cout << num << " is a happy number." << std::endl;
+    // } else {
+    //     std::cout << num << " is not a happy number." << std::endl;
+    // }
+
+    vector<int> arr = {5, 2, 3};
+    int target = 8;
+
+    vector<int> result = twosum(arr, target);
+    for(const auto& element: result){
+        cout << element << ",";
+    }
+    cout << endl;
+
+    return 0;
 }
