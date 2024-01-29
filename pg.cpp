77a7472b6217503d5b254da6ef0967995838627c
mamba <78519682+Intina47@users.noreply.github.com>
2024-01-29 23:19:45 +0000

    review maxprodSUb

--- pg.cpp
+++ pg.cpp
@@ -49,6 +49,27 @@ bool isHappy(int n) {
     return n == 1;
 }
 
+//Maximum Product Subarray
+//Given an array that contains both positive and negative integers, the task is to find the product of the maximum product subarray.
+//
+
+int maxSum(int arr[], int n){
+    int maxval = arr[0];
+    int minval = arr[0];
+    int maxprod = arr[0];
+    for(int i=0; i<n; ++i){
+        if(arr[i] < 0){
+            swap(maxval, minval);
+
+            maxval = max(arr[i], maxval*arr[i]);
+            minval = min(arr[i], minval*arr[i]);
+
+            maxprod = max(maxprod, maxval);
+        }
+    }
+    return maxprod;
+}
+
 int main() {
     // int num = 19;
 
