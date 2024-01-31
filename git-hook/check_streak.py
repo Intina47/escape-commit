6755f73084caf5284884fd29d93638070a048519
mamba <78519682+Intina47@users.noreply.github.com>
2024-01-31 18:01:23 +0000

    Fix streak count calculation in check_streak.py

--- git-hook/check_streak.py
+++ git-hook/check_streak.py
@@ -13,9 +13,10 @@ with open(os.path.join(home_dir, 'streak.txt'), 'r') as file:
     else:
         print("File is not empty")
         lines = file.readlines()
-        last_commit_date = datetime.datetime.strptime(lines[0].strip(), '%Y-%m-%d').date()
+        last_commit_date = datetime.datetime.strptime(lines[1].strip(), '%Y-%m-%d').date()
         if len(lines) > 1:
-            streak_count = int(lines[1])
+            streak_count = int(lines[0].strip())
+            print("streak count", streak_count)
         else:
             streak_count = 0
 
@@ -25,11 +26,11 @@ yesterday = today - datetime.timedelta(days=1)
 # If the last commit was yesterday, increment the streak count
 if last_commit_date == yesterday:
     streak_count += 1
-# Otherwise, reset the streak count
 else:
     streak_count = 0
 
-# Write the new streak count to streak.txt
+# Write the new streak count to streak.txt, keeping the rest of the file the same
 with open(os.path.join(home_dir, 'streak.txt'), 'w') as file:
-    file.write(str(today) + '\n')
-    file.write(str(streak_count))
+    file.write(str(streak_count) + '\n')  # Write the updated streak count
+    for line in lines[1:]:  # Write the rest of the lines back to the file
+        file.write(line)
