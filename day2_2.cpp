1e879800e2e1346c056a167179e4068e523cfd63
mamba <isaiahntina47@gmail.com>
2024-02-06 16:52:16 +0000

    first non- repeating char in a string

---
+++ day2_2.cpp
@@ -0,0 +1,31 @@
+// 387. First Unique Character in a String
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+ 
+// Example 1:=
+// Input: s = "leetcode"
+// Output: 0
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int firstUniqChar(string s){
+    unordered_map<char, int> map;
+    for(int i=0; i<s.length(); ++i){
+        map[s[i]]++;
+    }
+
+    for(int i=0; i<s.length(); ++i){
+        if(map[s[i]] == 1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    string input = "leetcode";
+    int res = firstUniqChar(input);
+    cout <<"Non repeating index is: "<< res << endl;
+
+    return 0;
+}
