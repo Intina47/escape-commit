878d95eeae5aa69a040be1302feac0368161b507
mamba <isaiahntina47@gmail.com>
2024-02-09 01:26:30 +0000

    fizz Buzz

---
+++ day3_2.cpp
@@ -0,0 +1,31 @@
+/**
+ * Fizz Buzz Problem states that given an integer n, 
+ * for every integer i <= n, 
+ * the task is to print “FizzBuzz” if i is divisible by 3 and 5, 
+ * “Fizz” if i is divisible by 3, 
+ * “Buzz” if i is divisible by 5 or i (as a string) 
+ * if none of the conditions are true.
+*/
+#include <iostream>
+using namespace std;
+
+void FizzBuzz(int n){
+    for(int i=1; i<=n; ++i){
+        if((i%15)==0){
+            cout << "FIZZBUZZ" <<endl;
+        }else if((i%3) == 0 && (i%5) != 0){
+            cout << "FIZZ" <<endl;
+        }else if((i%5) == 0 && (i%3) != 0){
+            cout << "BUZZ" <<endl;
+        }else{
+            cout << i <<endl;
+        }
+    }
+}
+
+int main(){
+    int num = 15;
+    FizzBuzz(num);
+
+    return 0;
+}
