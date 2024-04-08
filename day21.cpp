6a9bab21d55aa6b7d1e90813edbf25b1a21f2964
mamba <isaiahntina47@gmail.com>
2024-04-08 01:34:42 +0000

    Add runningSum function using std::vector in day21.cpp

--- day21.cpp
+++ day21.cpp
@@ -1,5 +1,6 @@
 // running sum of an array
 #include <iostream>
+#include <vector>
 
 int* runningSum(int arr[], int n){
     int current_sum = 0;
@@ -12,11 +13,22 @@ int* runningSum(int arr[], int n){
 
     return running_sum;
 }
+std::vector<int> runningsum(int arr[], int n){
+    int current_sum = 0;
+    std::vector<int> running_sum(n);
+
+    for(int i=0; i<n; ++i){
+        current_sum += arr[i];
+        running_sum[i] = current_sum;
+    }
+
+    return running_sum;
+}
 
 int main(){
     int arr[] = {1,2,3,4,5,6,7};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int* result = runningSum(arr,n);
+    std::vector<int> result = runningsum(arr,n);
 
     std::cout << "[";
     for(int i=0; i<n; ++i){
@@ -27,6 +39,6 @@ int main(){
     }
     std::cout << "]";
     std::cout << std::endl;
-    delete[] result;
+    // delete[] result;
     return 0;
 }
